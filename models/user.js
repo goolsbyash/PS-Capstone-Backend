bcfba5fbcb41d77ae59aaf5ef75c2958971d1750
goolsbyash <goolsbyashley015@gmail.com>
2024-02-10 15:03:02 -0500

    Added schema for users.

---
+++ models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  activePlan: {
+    type: String,
+    default: "",
+    required: false,
+  },
+});
+
+export default mongoose.model("User", userSchema);
