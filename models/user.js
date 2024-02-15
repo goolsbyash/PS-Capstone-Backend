991ee0a3ff2b13d58009ec04cd57fcd8b657cbc0
goolsbyash <goolsbyashley015@gmail.com>
2024-02-14 23:02:22 -0500

    Added mongoose-bcrypt and indexes.

--- models/user.js
+++ models/user.js
@@ -1,27 +1,49 @@
 import mongoose from "mongoose";
+import bcrypt from "mongoose-bcrypt";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: false,
+      default: "User",
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 22,
+      bcrypt: true,
+    },
+    activePlan: {
+      type: String,
+      default: "",
+      required: false,
+    },
   },
-  lastName: {
-    type: String,
-    required: false,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  activePlan: {
-    type: String,
-    default: "",
-    required: false,
-  },
-});
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, retDoc) {
+        delete retDoc.password; //removes password from the json doc
+        return retDoc;
+      },
+    },
+  }
+);
+
+userSchema.index({ email: 1 });
+userSchema.index({ activePlan: 1 });
+
+userSchema.plugin(bcrypt);
 
 export default mongoose.model("User", userSchema);
