086034b2d13eacbc7bc252b73994108b321ac3cb
goolsbyash <goolsbyashley015@gmail.com>
2024-02-15 00:21:10 -0500

    Added update route.

--- routes/users.js
+++ routes/users.js
@@ -14,20 +14,38 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// TODO: Route for updating account info
+router.patch("/:id/update", async (req, res) => {
+  const userId = req.params.id;
+  const newData = req.body;
+  console.log(newData);
+  try {
+    let updateUser = await User.findOneAndUpdate({ _id: userId }, newData, {
+      returnOriginal: false,
+    });
+    if (updateUser) {
+      console.log(updateUser);
+      res.status(200).json(updateUser);
+    } else {
+      res.status(304);
+      console.log("Update unsuccessful.");
+    }
+  } catch (error) {
+    console.log();
+  }
+});
 
 router.post("/signin", async (req, res) => {
   // user auth
-  const { email, password } = req.body;
-  User.verifyPassword(password, function (err, valid) {
-    if (err) console.log(err);
-    else if (valid) console.log("valid password");
-    else console.log("Invalid password");
-  });
+  // console.log(req.body);
+  // const checkUser = await User.create(req.body);
+  // console.log(checkUser);
+  // let compareUser = await User.findOne({ email: checkUser.email });
+  // if (compareUser) {
+  //   console.log(compareUser);
+  // }
 });
 
 router.post("/signup", async (req, res) => {
-  // const {firstName, lastName, email, password} = req.body;
   try {
     // check if email is registered in db
     if (await User.exists({ email: req.body.email })) {
